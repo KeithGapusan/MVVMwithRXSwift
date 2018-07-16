02f9d95a498133e0a3117ca29f127e958713e5e3
Keith Gapusan <keithgapusan@samcd-15015s-imac.local>
2018-07-16 15:43:52 +0800

    added the viewmodel class of movie list

---
+++ MVVMwithRXSwiftDemo/MVVMwithRXSwiftDemo/MovieListViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MovieListViewController.swift
+//  MVVMwithRXSwiftDemo
+//
+//  Created by Keith Gapusan on 16/07/2018.
+//  Copyright © 2018 Keith Gapusan. All rights reserved.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet var viewModel: MovieViewModel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
+extension MovieListViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfItemsInSection(section: section)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
+        configureCell(cell: cell, indexPath: indexPath)
+        return cell
+    }
+    
+    func configureCell(cell: UITableViewCell , indexPath : IndexPath){
+        cell.textLabel?.text = "hello"
+    }
+    
+}
---
+++ MVVMwithRXSwiftDemo/MVVMwithRXSwiftDemo/MovieViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  MovieViewModel.swift
+//  MVVMwithRXSwiftDemo
+//
+//  Created by Keith Gapusan on 16/07/2018.
+//  Copyright © 2018 Keith Gapusan. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewModel: NSObject {
+
+    
+    func numberOfItemsInSection(section: Int) -> Int{
+        return 10
+    }
+    
+    
+    
+}
